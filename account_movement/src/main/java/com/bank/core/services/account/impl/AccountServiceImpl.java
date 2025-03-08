e44b06f5e85e9edce7bd61a3028045068ec777b4
Andres Silva <rsilva2014@live.com>
2025-03-08 18:04:57 -0500

    feature: added logs on the service

--- account_movement/src/main/java/com/bank/core/services/account/impl/AccountServiceImpl.java
+++ account_movement/src/main/java/com/bank/core/services/account/impl/AccountServiceImpl.java
@@ -31,49 +31,83 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public ResponseEntity<AccountDTO> create(AccountDTO accountDTO) {
         Account account = this.accountMapper.dtoToEntity(accountDTO);
+        log.info("Processing Service create for, AccountNumber={}, serviceMethod={}",
+                accountDTO.getAccountNumber(), "create");
         Optional<Account> optionalAccount = Optional.ofNullable(this.accountRepository.save(account));
-        if(optionalAccount.isPresent()){
-            return new ResponseEntity<>( this.accountMapper.entityToDTO(optionalAccount.get()),
+        if (optionalAccount.isPresent()) {
+            log.info("Valid create request for, AccountNumber={id}, serviceMethod={}",
+                    accountDTO.getAccountNumber(), "create");
+            return new ResponseEntity<>(this.accountMapper.entityToDTO(optionalAccount.get()),
                     HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>( new AccountDTO(), HttpStatus.BAD_REQUEST);
+        } else {
+            log.info("No contend for create, AccountNumber={}, serviceMethod={}",
+                    accountDTO.getAccountNumber(), "update");
+            return new ResponseEntity<>(new AccountDTO(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Override
     public ResponseEntity<List<AccountDTO>> getAll() {
         List<Account> accountDTOList = this.accountRepository.findAll();
-        if(!accountDTOList.isEmpty()){
+        log.info("Processing Service get all for, serviceMethod={}", "getAll");
+        if (!accountDTOList.isEmpty()) {
+            log.info("Valid update request for, serviceMethod={}", "getAll");
             return new ResponseEntity<>(accountDTOList.stream()
                     .map(accountMapper::entityToDTO)
                     .toList(), HttpStatus.OK);
         } else {
-          return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NO_CONTENT);
+            log.info("No contend for update, serviceMethod={}",  "getAll");
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NO_CONTENT);
         }
     }
 
     @Override
     public ResponseEntity<AccountDTO> getAccountById(Long id) {
         Optional<Account> client = this.accountRepository.findById(id);
-        if(client.isPresent()){
-            return new ResponseEntity<>( this.accountMapper.entityToDTO(client.get()),
+        log.info("Processing Service get by id, id={},  serviceMethod={}", id,"getAccountById");
+        if (client.isPresent()) {
+            log.info("Valid get by id request for, id={}, serviceMethod={}", id,"getAll");
+            return new ResponseEntity<>(this.accountMapper.entityToDTO(client.get()),
                     HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>( new AccountDTO(), HttpStatus.BAD_REQUEST);
+        } else {
+            log.info("No contend for get by id, id={}, serviceMethod={}", id, "getAll");
+            return new ResponseEntity<>(new AccountDTO(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Override
     public ResponseEntity<AccountDTO> update(Long id, AccountDTO accountDTO) {
-        Optional<Account> update  = this.accountRepository.findById(id);
-        if(update.isPresent()){
+        Optional<Account> update = this.accountRepository.findById(id);
+        log.info("Processing Service update for, id={},  serviceMethod={}", id,"update");
+        if (update.isPresent()) {
             Account account = buildAccount(accountDTO, update.get().getId());
             Optional<Account> accountUpdated =
-            Optional.ofNullable(this.accountRepository.save(account));
+                    Optional.ofNullable(this.accountRepository.save(account));
+            log.info("Valid update request for, id={}, serviceMethod={}", id,"update");
             return new ResponseEntity<>(this.accountMapper.entityToDTO(accountUpdated.get()),
-                    HttpStatus.BAD_REQUEST);
+                    HttpStatus.OK);
         } else {
-            return new ResponseEntity<>( new AccountDTO(), HttpStatus.BAD_REQUEST);
+            log.info("No contend for update, id={}, serviceMethod={}", id, "update");
+            return new ResponseEntity<>(new AccountDTO(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseDTO> delete(Long id) {
+        Optional<Account> account = this.accountRepository.findById(id);
+        log.info("Processing Service delete for, id={},  serviceMethod={}", id, "delete");
+        if (account.isPresent()) {
+            String accountNumber = account.get().getAccountNumber();
+            this.accountRepository.delete(account.get());
+            log.info("Valid delete request for, id={}, serviceMethod={}", id, "delete");
+            return new ResponseEntity<>(ResponseDTO.builder()
+                    .message("Account " + accountNumber + " was deleted successfully.")
+                    .build(), HttpStatus.OK);
+        } else {
+            log.info("No contend for delete, id={}, serviceMethod={}", id, "delete");
+            return new ResponseEntity<>(ResponseDTO.builder()
+                    .message("Account does not exist")
+                    .build(), HttpStatus.NO_CONTENT);
         }
     }
 
@@ -87,21 +121,5 @@ public class AccountServiceImpl implements AccountService {
                 .status(accountDTO.getStatus())
                 .build();
 
-    }
-
-    @Override
-    public ResponseEntity<ResponseDTO> delete(Long id) {
-        Optional<Account> account = this.accountRepository.findById(id);
-        if(account.isPresent()) {
-            String accountNumber = account.get().getAccountNumber();
-            this.accountRepository.delete(account.get());
-            return new ResponseEntity<>(ResponseDTO.builder()
-                    .message("Account "+accountNumber+" was deleted successfully")
-                    .build(), HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>(ResponseDTO.builder()
-                    .message("Account does not exist")
-                    .build(), HttpStatus.NO_CONTENT);
-        }
     }
 }
--- account_movement/src/main/java/com/bank/core/services/movement/impl/MovementServiceImpl.java
+++ account_movement/src/main/java/com/bank/core/services/movement/impl/MovementServiceImpl.java
@@ -38,6 +38,8 @@ public class MovementServiceImpl implements MovementService {
     @Override
     public ResponseEntity<MovementDTO> create(MovementDTO movementDTO)  {
         Movement movement = this.movementMapper.dtoToEntity(movementDTO);
+        log.info("Processing Service create for AccountNumber={}, serviceMethod={}",
+                movementDTO.getAccount().getAccountNumber(), "create");
         Optional<Account> account= this.accountRepository.findById(movement.getAccount().getId());
         if(account.isPresent()) {
             Account accountToUpdate = account.get();
@@ -50,18 +52,26 @@ public class MovementServiceImpl implements MovementService {
                 movement.setAccount(accountToUpdate);
                 movement.setCreatedDate(movement.getTransferDate());
                 Optional<Movement> movement1= Optional.ofNullable(this.movementRepository.save(movement));
+                log.info("Valid request for AccountNumber={}, serviceMethod={}",
+                        movementDTO.getAccount().getAccountNumber(), "create");
                 return new ResponseEntity<>( this.movementMapper.entityToDTO(
                         movement1.get()), HttpStatus.OK);
             } else if(accountToUpdate.getStatus().equals(false)){
+                log.info("Not Valid request, Account disabled AccountNumber={}, serviceMethod={}",
+                        movementDTO.getAccount().getAccountNumber(), "create");
                 String message = "You cannot transfer to an enactive account:"+ accountToUpdate.getAccountNumber();
                 throw new BadRequest(message, new Throwable("You cannot transfer to an enactive account"));
             }else {
+                log.info("Not Valid request, Account insufficient funds AccountNumber={}, serviceMethod={}",
+                        movementDTO.getAccount().getAccountNumber(), "create");
                 String message = "You have insufficient funds, your Balance is :"+ accountToUpdate.getInitialBalance()
                         +"  and your transaction is: " +movementDTO.getTransferAmount();
 
                 throw new BadRequest(message, new Throwable("You have insufficient funds"));
             }
         }
+        log.info("Bad request, AccountNumber={}, serviceMethod={}",
+                movementDTO.getAccount().getAccountNumber(), "create");
             return new ResponseEntity<>( new MovementDTO(), HttpStatus.BAD_REQUEST);
     }
 
@@ -69,11 +79,14 @@ public class MovementServiceImpl implements MovementService {
     @Override
     public ResponseEntity<List<MovementDTO>> getAll() {
         List<Movement> movementList = this.movementRepository.findAll();
+        log.info("Processing Service get all for, serviceMethod={}",  "getAll");
         if(!movementList.isEmpty()){
+            log.info("Valid request for get all, serviceMethod={}",  "getAll");
             return new ResponseEntity<>(movementList.stream()
                     .map(movementMapper::entityToDTO)
                     .toList(), HttpStatus.OK);
         }else {
+            log.info("No contend for, serviceMethod={}",  "getAll");
            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
@@ -81,10 +94,13 @@ public class MovementServiceImpl implements MovementService {
     @Override
     public ResponseEntity<MovementDTO> getMovementById(Long id) {
         Optional<Movement> movement = this.movementRepository.findById(id);
+        log.info("Processing Service for get movement by Id={}, serviceMethod={}",  id,"getMovementById");
         if(movement.isPresent()){
+            log.info("Valid Request for get movement by Id={}, serviceMethod={}",  id,"getMovementById");
             return new ResponseEntity<>(this.movementMapper
                     .entityToDTO(movement.get()), HttpStatus.OK);
         }else{
+            log.info("No contend for, Id={}, serviceMethod={}",  id,"getMovementById");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
@@ -93,13 +109,16 @@ public class MovementServiceImpl implements MovementService {
     public ResponseEntity<MovementDTO> update(Long id, MovementDTO movementDTO) {
         Optional<Movement> update  = this.movementRepository.findById(id);
         Optional<Account> account  = this.accountRepository.findById(movementDTO.getAccount().getId());
+        log.info("Processing Service update for, Id={}, serviceMethod={}",  id,"update");
         if(update.isPresent() && account.isPresent()){
             Movement dataUpdated = buildMovement(movementDTO, update.get().getId(), account.get());
             Optional <Movement> updatedMovement = Optional.ofNullable(this.movementRepository.save(dataUpdated));
+            log.info("Valid update Request for, id={id}, serviceMethod={}",  id,"update");
             return new ResponseEntity<>(this.movementMapper
                     .entityToDTO(updatedMovement.get()),HttpStatus.OK);
 
         }else {
+            log.info("No contend for update, Id={}, serviceMethod={}",  id,"update");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
@@ -120,13 +139,16 @@ public class MovementServiceImpl implements MovementService {
     @Override
     public ResponseEntity<ResponseDTO> delete(Long id) {
         Optional<Movement> movement = this.movementRepository.findById(id);
+        log.info("Processing Service delete for, Id={}, serviceMethod={}",  id,"delete");
         if(movement.isPresent()){
             String accountNumber = movement.get().getAccount().getAccountNumber();
             this.movementRepository.delete(movement.get());
+            log.info("Valid delete request for, id={id}, serviceMethod={}",  id,"delete");
             return new ResponseEntity<>(ResponseDTO.builder()
                     .message("Movements for account:"+accountNumber+", transaction id "+id+" was deleted successfully")
                     .build(), HttpStatus.OK);
         }else {
+            log.info("No contend for delete, Id={}, serviceMethod={}",  id,"update");
             return new ResponseEntity<>(ResponseDTO.builder()
                     .message("Movements does not exist")
                     .build(), HttpStatus.NO_CONTENT);
